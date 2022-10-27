7022f61588123a123b5a4f7463ea99e7d22bb608
Beau-Yang <49222826+Beau-Yang@users.noreply.github.com>
2022-10-27 19:52:42 +0800

    Add files via upload
    Pull & Check

---
+++ PullAndCheck.py
@@ -0,0 +1,102 @@
+import os
+import sys
+from pathlib import Path
+
+import cv2
+from adb import adb_commands, sign_m2crypto
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]
+
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))
+
+data_dir_path = ROOT.joinpath("Data")
+
+# Enable USB Debugging mode of your mobile phone
+
+# Authentication and Connect to the device
+signer = sign_m2crypto.M2CryptoSigner(os.path.expanduser("~/.android/adbkey"))
+device = adb_commands.AdbCommands()
+device.ConnectDevice(rsa_keys=[signer])
+
+
+def copy_file(src_path, dst_path):
+
+    data_bytes = device.Pull(src_path)
+    with open(dst_path, "wb") as f:
+        f.write(data_bytes)
+
+
+def copy():
+    base_dir_path = "/storage/emulated/0/DCIM/OpenCamera/"
+    data_dir_path = str(data_dir_path)
+
+    files = device.List(base_dir_path)
+    for file in files:
+        file_name = getattr(file, "filename").decode("utf-8")
+
+        if "." in file_name:
+            # file: jpg / mp4 / srt
+            copy_file(base_dir_path + file_name, data_dir_path + "/" + file_name)
+        else:
+            # dir
+            Path(data_dir_path + "/" + file_name).mkdir(exist_ok=True)
+
+            for sub_file in device.List(base_dir_path + file_name):
+                sub_file_name = getattr(sub_file, "filename").decode("utf-8")
+                copy_file(
+                    base_dir_path + file_name + "/" + sub_file_name,
+                    data_dir_path + "/" + file_name + "/" + sub_file_name,
+                )
+
+
+class FileSizeTooSmallError(Exception):
+    """raised when file size is too small for duration of video"""
+
+    pass
+
+
+class TimestampDurationMatchError(Exception):
+    """raised when timestamp duration does not match duration of video"""
+
+    pass
+
+
+def check_filesize(vid_path: str):
+    cap = cv2.VideoCapture(vid_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    duration = frame_count / fps
+    cap.release()
+    file_byte = os.path.getsize(vid_path)
+    bitrate = 20000000
+    expected_size = bitrate * duration / 8
+    if file_byte < expected_size * 0.8:
+        raise FileSizeTooSmallError
+
+
+def check_timestamp(vid_path: str, csv_path: str):
+    f = open(csv_path, "r")
+    lines = f.readlines()
+    f.close()
+    start_timestamp, end_timestamp = lines[0], lines[-1]
+    cap = cv2.VideoCapture(vid_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count / fps
+    timestamp_duration = (int(end_timestamp) - int(start_timestamp)) / pow(10, 9)
+    if round(duration, 1) != round(timestamp_duration, 1):
+        raise TimestampDurationMatchError
+
+
+if __name__ == "__main__":
+    copy()
+    check_filesize(
+        "/home/yangbo/Documents/Internal/Blurring/Blur/license-plate-blurring/Data/VID_20221016_143907.mp4"
+    )
+    check_timestamp(
+        "/home/yangbo/Documents/Internal/Blurring/Blur/license-plate-blurring/Data/VID_20221016_164644.mp4",
+        "/home/yangbo/Documents/Internal/Blurring/Blur/license-plate-blurring/Data/20221016_164644/VID_20221016_164644_imu_timestamps.csv",
+    )
